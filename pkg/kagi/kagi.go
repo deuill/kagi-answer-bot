0ae82110b428fcc205481c104aab0fde978110ea
Marid de Uill <code@deuill.org>
2026-08-20 11:15:13 +0200

    Update for breaking changes to Answer API
    Kagi Quick Answers now requires that search results are sent through
    with the query made, and has updated other aspects of the API surface to
    account for these requirements; this commit updates integration with
    Kagi in order to restore functionality.

--- pkg/kagi/kagi.go
+++ pkg/kagi/kagi.go
@@ -9,10 +9,9 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"mime"
 	"net/http"
 	"net/url"
-	"strings"
+	"time"
 
 	// Third-party packages
 	"github.com/go-joe/joe"
@@ -21,11 +20,9 @@ import (
 
 // Error messages.
 var (
-	errNoClientBot       = errors.New("no valid bot instance assigned to client")
-	errEmptyLoginToken   = errors.New("non-empty login token required for client")
-	errAnswerEmptyQuery  = errors.New("empty query given for answer")
-	errAnswerRequest     = errors.New("failed making HTTP request for answers")
-	errParseAnswerStream = errors.New("unable to parse streaming response body")
+	errNoClientBot      = errors.New("no valid bot instance assigned to client")
+	errEmptyLoginToken  = errors.New("non-empty login token required for client")
+	errAnswerEmptyQuery = errors.New("empty query given for answer")
 )
 
 // Bot response messages.
@@ -38,13 +35,20 @@ const (
 const (
 	// General-use values.
 	sessionCookieName = "kagi_session"          // The cookie name used for cookie-based authentication against Kagi.
-	defaultUserAgent  = "kagi-answer-bot/0.1.0" // The HTTP user agent to use when making requests against Kagi.
+	defaultUserAgent  = "kagi-answer-bot/0.2.0" // The HTTP user agent to use when making requests against Kagi.
+	maxBufferSize     = 1024 * 1024
 
-	// Values for Kagi Answers integration.
-	answerBaseURL        = "https://kagi.com/mother/context" // The base URL for Kagi Quick Answer HTTP requests.
-	answerQueryParam     = "q"                               // The query parameter name used for Kagi Quick Answer queries.
-	answerContentType    = "application/vnd.kagi.stream"     // The custom content-type used for Kagi Quick Answer responses.
-	answerDocumentPrefix = "new_message.json"                // The prefix used for locating Answer documents in streaming responses.
+	// Values for Kagi Search.
+	searchBaseURL    = "https://kagi.com/socket/search?q=%s"
+	searchDataPrefix = "data:"
+	searchResultsTag = "search_results_json"
+
+	// Values for Kagi Quick Answer.
+	answerBaseURL = "https://assistant-api.kagi.com/mother/context"
+	answerPrefix  = "final:"
+
+	// The amount of time we'll wait before cancelling an in-flight request.
+	defaultRequestTimeout = 10 * time.Second
 )
 
 // A Client represents a method of making calls to Kagi API endpoints, in support of bot interactions.
@@ -54,9 +58,9 @@ type Client struct {
 	userAgent  string
 
 	// Other internal fields.
-	bot       Bot
-	transport http.RoundTripper
-	logger    *zap.Logger
+	bot    Bot
+	client http.Client
+	logger *zap.Logger
 }
 
 // A Bot represents an automated user that provides the ability of pushing (or "saying") plain-text
@@ -107,8 +111,11 @@ func WithLogger(logger *zap.Logger) ClientOption {
 func NewClient(options ...ClientOption) (*Client, error) {
 	var c = &Client{
 		userAgent: defaultUserAgent,
-		transport: http.DefaultTransport,
-		logger:    zap.NewNop(),
+		client: http.Client{
+			Transport: http.DefaultTransport,
+			Timeout:   defaultRequestTimeout,
+		},
+		logger: zap.NewNop(),
 	}
 
 	for _, fn := range options {
@@ -146,12 +153,6 @@ func (c *Client) HandleEvent(ctx context.Context, e joe.ReceiveMessageEvent) err
 	return nil
 }
 
-// AnswerResponse represents structured data returned in response to the Kagi Quick Answer API.
-type answerResponse struct {
-	Markdown           string `json:"md"`
-	MarkdownReferences string `json:"references_md"`
-}
-
 // Fetch a quick answer from Kagi, as synthesized from multiple sources. The result will be a Markdown
 // encoded string if the query was successful, or an error if not.
 func (c *Client) Answer(ctx context.Context, query string) (string, error) {
@@ -159,81 +160,160 @@ func (c *Client) Answer(ctx context.Context, query string) (string, error) {
 		return "", errAnswerEmptyQuery
 	}
 
-	u := fmt.Sprintf("%s?%s=%s", answerBaseURL, answerQueryParam, url.QueryEscape(query))
-	req, err := http.NewRequestWithContext(ctx, "POST", u, nil)
+	// Fetch search results for query.
+	body, err := c.sendRequest(ctx, http.MethodGet, fmt.Sprintf(searchBaseURL, url.QueryEscape(query)), nil)
 	if err != nil {
-		return "", fmt.Errorf("%w: %w", errAnswerRequest, err)
+		return "", fmt.Errorf("sending search request: %w", err)
 	}
 
-	req.Header.Add("Cookie", fmt.Sprintf("%s=%s", sessionCookieName, c.loginToken))
-	req.Header.Add("Accept", answerContentType)
-	req.Header.Add("User-Agent", c.userAgent)
-
-	h := &http.Client{Transport: c.transport}
-	resp, err := h.Do(req)
+	result, err := c.parseSearchResult(body)
 	if err != nil {
-		return "", fmt.Errorf("%w: %w", errAnswerRequest, err)
-	} else if resp.StatusCode >= 400 && resp.StatusCode <= 599 {
-		return "", fmt.Errorf("%w: %s received from server", errAnswerRequest, resp.Status)
+		return "", fmt.Errorf("parsing search response: %w", err)
 	}
 
-	defer resp.Body.Close() //nolint:errcheck
-	var answer answerResponse
-
-	contentType := resp.Header.Get("Content-Type")
-	mediaType, _, _ := mime.ParseMediaType(contentType)
-
-	switch mediaType {
-	case "text/html":
-		answer, err = parseAnswerStream(resp.Body)
-		if err != nil {
-			return "", fmt.Errorf("%w: failed parsing streaming answer: %w", errAnswerRequest, err)
-		}
-	case "application/json":
-		if err = json.NewDecoder(resp.Body).Decode(&answer); err != nil {
-			return "", fmt.Errorf("%w: %w", errAnswerRequest, err)
-		}
-	default:
-		return "", fmt.Errorf("%w: unknown content type: %s", errAnswerRequest, contentType)
+	// Synthesize quick answer from search results and original query.
+	req := answerRequest{Query: query, searchResult: *result}
+	body, err = c.sendRequest(ctx, http.MethodPost, answerBaseURL, req)
+	if err != nil {
+		return "", fmt.Errorf("sending answer request: %w", err)
 	}
 
-	result := answer.Markdown
-	if answer.MarkdownReferences != "" {
-		result += "\n---\nReferences:\n" + answer.MarkdownReferences
+	answer, err := c.parseAnswerResult(body)
+	if err != nil {
+		return "", fmt.Errorf("parsing answer response: %w", err)
 	}
 
-	return strings.TrimSpace(result), nil
+	return answer, nil
 }
 
-// ParseAnswerStream parses the a streamed Kagi Answer response from the given [io.Reader], returning
-// a valid response, or an error if none could be parsed.
-func parseAnswerStream(r io.Reader) (answerResponse, error) {
-	var scanner = bufio.NewScanner(r)
-	var answer answerResponse
+// SendRequest processes and sends an HTTP request for the given HTTP method and URL, with optional,
+// JSON-encoded body data. Successful calls will have the response body returned as an [io.ReadCloser],
+// with callers being responsible for reading and closing the response correctly.
+func (c *Client) sendRequest(ctx context.Context, method, url string, data any) (io.ReadCloser, error) {
+	var body bytes.Buffer
+	if data != nil {
+		if err := json.NewEncoder(&body).Encode(data); err != nil {
+			return nil, fmt.Errorf("encoding request body to JSON: %w", err)
+		}
+	}
+
+	req, err := http.NewRequestWithContext(ctx, method, url, &body)
+	if err != nil {
+		return nil, fmt.Errorf("initializing request: %w", err)
+	}
+
+	c.logger.Debug("sending HTTP request", zap.String("method", method), zap.String("url", url))
+
+	req.Header.Add("Cookie", fmt.Sprintf("%s=%s", sessionCookieName, c.loginToken))
+	req.Header.Add("User-Agent", c.userAgent)
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("sending request: %w", err)
+	} else if resp.StatusCode >= 400 && resp.StatusCode <= 599 {
+		return nil, fmt.Errorf("unexpected %s status received", resp.Status)
+	}
+
+	return resp.Body, nil
+}
+
+// SearchResponse represents data returned by the Kagi Search socket API.
+type searchResponse struct {
+	Tag     string `json:"tag"`
+	Payload string `json:"payload"`
+	Version string `json:"kagi_version"`
+}
+
+// SearchResult represents the payload for search results returned in [SearchResponse] values.
+type searchResult struct {
+	Items []struct {
+		Title   string `json:"title"`
+		URL     string `json:"url"`
+		Snippet string `json:"snippet"`
+		Rank    int64  `json:"rank"`
+		Source  string `json:"source"`
+	} `json:"items"`
+	AnswerBox any `json:"answer_box"`
+}
+
+// ParseSearchResult processes the given response body, intended to correspond to a search query on
+// the Kagi socket API, and returns a concrete [searchResult] on successful calls. The [io.ReadCloser]
+// is closed after calls to this function, and cannot be reused.
+func (c *Client) parseSearchResult(body io.ReadCloser) (*searchResult, error) {
+	var scanner = bufio.NewScanner(body)
+	scanner.Buffer([]byte{}, maxBufferSize)
+
+	defer body.Close() //nolint:errcheck
 
 	for scanner.Scan() {
-		suffix, ok := bytes.CutPrefix(scanner.Bytes(), []byte(answerDocumentPrefix+":"))
+		data, ok := bytes.CutPrefix(scanner.Bytes(), []byte(searchDataPrefix))
 		if ok {
-			suffix = bytes.TrimFunc(suffix, isInvalidByte)
-			if err := json.Unmarshal(suffix, &answer); err != nil {
-				return answerResponse{}, err
+			var resp []searchResponse
+			data = bytes.Trim(data, "\x00")
+			if err := json.Unmarshal(data, &resp); err != nil {
+				continue
+			}
+			var result searchResult
+			for _, r := range resp {
+				if r.Tag == searchResultsTag {
+					if err := json.Unmarshal([]byte(r.Payload), &result); err != nil {
+						return nil, fmt.Errorf("parsing search result payload: %w", err)
+					}
+					return &result, nil
+				}
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return nil, fmt.Errorf("no search results found in response")
+}
+
+// AnswerRequest represents the body for HTTP requests sent to the Kagi Answer API.
+type answerRequest struct {
+	Query string `json:"query"`
+	searchResult
+}
+
+// AnswerResponse represents the response body for HTTP requests sent to the Kagi Answer API.
+type answerResponse struct {
+	Data struct {
+		Markdown           string `json:"markdown"`
+		MarkdownReferences string `json:"md_references"`
+	} `json:"output_data"`
+}
+
+// ParseAnswerResult processes the given response body, intended to correspond to a answer query on
+// the Kagi Answer API, and returns a concrete [searchResult] on successful calls. The [io.ReadCloser]
+// is closed after calls to this function, and cannot be reused.
+func (c *Client) parseAnswerResult(body io.ReadCloser) (string, error) {
+	var scanner = bufio.NewScanner(body)
+	scanner.Buffer([]byte{}, maxBufferSize)
+
+	defer body.Close() //nolint:errcheck
+
+	for scanner.Scan() {
+		data, ok := bytes.CutPrefix(scanner.Bytes(), []byte(answerPrefix))
+		if ok {
+			var resp answerResponse
+			data = bytes.Trim(data, "\x00")
+			if err := json.Unmarshal(data, &resp); err != nil {
+				return "", fmt.Errorf("parsing answer payload: %w", err)
+			}
+			answer := resp.Data.Markdown
+			if resp.Data.MarkdownReferences != "" {
+				answer += "\n---\nReferences:\n" + resp.Data.MarkdownReferences
 			}
 			return answer, nil
 		}
 	}
 
 	if err := scanner.Err(); err != nil {
-		return answerResponse{}, err
+		return "", err
 	}
 
-	return answerResponse{}, errParseAnswerStream
-}
-
-// IsInvalidByte is used in trimming invalid bytes from streamed responses.
-func isInvalidByte(r rune) bool {
-	switch r {
-	case '\x00':
-		return true
-	}
-	return false
+	return "", fmt.Errorf("no answer found in response")
 }
